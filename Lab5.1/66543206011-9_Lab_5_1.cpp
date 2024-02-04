325041298d2c525d39ab81a491213f0c74c2464b
Tanakan Kaewpa <141503199+tanakan001@users.noreply.github.com>
2024-02-04 20:45:25 +0700

    Update 66543206011-9_Lab_5_1.cpp

--- Lab5.1/66543206011-9_Lab_5_1.cpp
+++ Lab5.1/66543206011-9_Lab_5_1.cpp
@@ -1,93 +1,82 @@
 #include <stdio.h>
 #include <string.h>
 
-struct StudentNode {
+struct studentNode {
     char name[20];
     int age;
     char sex;
     float gpa;
-    StudentNode* next;
+    struct studentNode *next;
 };
 
-void showAll(StudentNode* walk);
-
-StudentNode* AddNode(StudentNode** start, const char name[], int age, char sex, float gpa);
-void InsNode(StudentNode* now, const char name[], int age, char sex, float gpa);
-void DelNode(StudentNode* now);
+void DelNode(struct studentNode **walk );
+void InsNode(struct studentNode **walk, char name[], int age, char sex, float gpa, struct studentNode **begin);
+void showAll(struct studentNode *walk);
+struct studentNode *AddNode(struct studentNode **walk, char name[], int age, char sex, float gpa);
 
 int main() {
-    StudentNode* start = nullptr;
-    StudentNode* now = AddNode(&start, "one", 6, 'M', 3.11);
-    showAll(start);
-
-    now = AddNode(&start, "two", 8, 'F', 3.22);
-    showAll(start);
-
-    InsNode(now, "three", 10, 'M', 3.33);
-    showAll(start);
-
-    InsNode(now, "four", 12, 'F', 3.44);
-    showAll(start);
-
-    DelNode(now);
-    showAll(start);
-
+	
+    struct studentNode *start, *now;
+    start = NULL;
+    now = AddNode(&start, "one", 6, 'M', 3.11);		showAll(start);
+    now = AddNode(&start, "two", 8, 'F', 3.22);		showAll(start);
+    InsNode(&now, "three", 10, 'M', 3.33, &start);	showAll(start);
+    InsNode(&now, "four", 12, 'F', 3.44, &start);	showAll(start);
+    DelNode(&now);	showAll(start);
     return 0;
+    
 }
-
-void showAll(StudentNode* walk) {
-    while (walk != nullptr) {
-        printf("%s ", walk->name);
+void showAll(struct studentNode *walk) {
+    while (walk != NULL) {
+        printf( "%s " , walk->name );
         walk = walk->next;
     }
     printf("\n");
 }
 
-StudentNode* AddNode(StudentNode** start, const char name[], int age, char sex, float gpa) {
-    StudentNode* newNode = new StudentNode;
-
-    strncpy(newNode->name, name, sizeof(newNode->name));
-    newNode->age = age;
-    newNode->sex = sex;
-    newNode->gpa = gpa;
-    newNode->next = nullptr;
-
-    if (*start == nullptr) {
-        *start = newNode;
-    } else {
-        StudentNode* walk = *start;
-        while (walk->next != nullptr) {
-            walk = walk->next;
-        }
-        walk->next = newNode;
-    }
-
-    return newNode;
+struct studentNode *AddNode(struct studentNode **walk, char name[], int age, char sex, float gpa) {
+	struct studentNode *temp = NULL;
+	while( *walk != NULL){
+		walk = &(*walk)->next;
+	}
+    *walk = new struct studentNode;
+    strcpy((*walk)->name, name);
+    (*walk)->age = age;
+    (*walk)->sex = sex;
+    (*walk)->gpa = gpa;
+    (*walk)->next = NULL;
+	return *walk;
 }
 
-void InsNode(StudentNode* now, const char name[], int age, char sex, float gpa) {
-    if (now == nullptr) {
-        fprintf(stderr, "Invalid insertion point.\n");
-        return;
-    }
-
-    StudentNode* newNode = new StudentNode;
-
-    strncpy(newNode->name, name, sizeof(newNode->name));
-    newNode->age = age;
-    newNode->sex = sex;
-    newNode->gpa = gpa;
-    newNode->next = now->next;
-    now->next = newNode;
+void InsNode(struct studentNode **walk, char name[], int age, char sex, float gpa, struct studentNode **begin) {
+	
+	if( walk != NULL ){
+	struct studentNode *temp = new struct studentNode;
+    strcpy(temp->name, name);
+    temp->age = age;
+    temp->sex = sex;
+    temp->gpa = gpa;
+    temp->next = (*walk)->next;
+    (*walk)->next = temp;
+	}
+    else{
+    *begin = new struct studentNode;
+    strcpy((*begin)->name, name);
+    (*begin)->age = age;
+    (*begin)->sex = sex;
+    (*begin)->gpa = gpa;
+    (*begin)->next = *walk;
+    *walk = *begin;
+	}
 }
 
-void DelNode(StudentNode* now) {
-    if (now == nullptr || now->next == nullptr) {
-        fprintf(stderr, "Invalid deletion point.\n");
-        return;
-    }
+void DelNode(struct studentNode **walk) {
+    struct studentNode *temp;
 
-    StudentNode* temp = now->next;
-    now->next = temp->next;
-    delete temp;
+    if (*walk != NULL) {
+    	temp = (*walk)->next;
+        DelNode(&((*walk)->next));
+        delete *walk;
+        *walk = temp;
+    }
 }
